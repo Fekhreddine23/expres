12bacf9fa29251f465a72cd5233b48196c9347ef
Fekhreddine23 <fekhredine1990@gmail.com>
2022-12-22 16:03:18 +0100

    creation fichiers html, creation des routes

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-const esprex = require('./core/esprex');
-const app = esprex();
+const express = require('express');
+const { resolve } = require('path');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.end('Home Page');
-});
-
-app.get('/', (req, res) => {
-    res.end(`
-      <h1>Home Page</h1>
-      <form method="POST" action="/contact">
-        <input type="text" name="email">
-        <button type="submit">Go !</button>
-      </form>
-    `);
-  });
-
-
-
-app.get('/contact', (req, res) => {
-  res.end('Contact Page');
-});
-
-
-app.get('/services', (req, res) => {
-    res.end('Services');
-  });   
-
-
-  
-
- // POST method route
-app.post('/avis', (req, res) => {
-    res.end('Contact Page (POST)'); //fonction get a un chemin et une fonction de callback
-  });  
-    
-  
 module.exports = app;
+
+
+//CONFIGURATION GENERALE
+app.use(express.static(resolve('public')));
+
+
+//ROUTING
+
+router.get('/api/users', function(req,res){
+    const data = require('./database/data.json')
+    res.json({
+        nbrOfUsers : data.users.length,
+        users: data.users
+    });
+});
+/**1/ Créer un dossier public/ dans lequel on aura:
+  - Un fichier pour l'accueil (index.html)
+  - Un fichier pour la page de services
+  - Un fichier pour la page de contact
+
+2/ Créer les routes en GET permettant de charger les fichier */
+router.get('/contact', function (req, res) {
+    res.sendFile(path.resolve('public', 'contact.html'));
+});
+
+router.get('/services', function (req, res) {
+    res.sendFile(path.resolve('public', 'services.html'));
+});
+
+router.get('*', function (req, res) { // * route par defaut TOUJOURS DECLARER A LA FIN
+    res.sendFile(path.resolve('public', 'index.html'));
+     
+});
+
+router.get('*', function (req, res) { // * route par defaut TOUJOURS DECLARER A LA FIN
+    res.sendFile(path.resolve('public', 'page404.html'));
+     
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
