d442a9565954c4f7df8aeb217d6b08dbceda4bab
Fekhreddine23 <fekhredine1990@gmail.com>
2022-12-23 14:31:38 +0100

    affiche des users du json : Fonctionnel

--- routing/index.js
+++ routing/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router(); //initialise le router
 const appRouter = require('./app-routes.js'); //appelle les fichiers routers
 
 const apiRouter = require('./api-app-routes.js')
-const {controllerDefault} = require('../controllers/controller')
+const {controllerDefault} = require('../controllers/controller') //destructuration de la methode controllerDefault
 
 router.use(appRouter); //je fais le lien entre objet router et le fichier app-routes.js
 
 router.use(apiRouter);
 
-router.get('*',  controllerDefault );
+router.get('*',  controllerDefault ); //passe a la fin les routes par defauts
 
 module.exports = router;
