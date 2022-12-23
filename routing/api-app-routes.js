d72e2952624a50a67f9f0151ac90706c7b87a016
Fekhreddine23 <fekhredine1990@gmail.com>
2022-12-23 10:13:47 +0100

    controllers et routes toutes fonctionnels

--- routing/api-app-routes.js
+++ routing/api-app-routes.js
@@ -2,7 +2,7 @@ const { resolve } = require('path');
 const router = require('express').Router();
 
 
-const controllerAccueil = require('../controllers/api-controller.js');
+const {controllerAccueil} = require('../controllers/api-controller.js'); //destructuration methode controllerAccueil
 
 router.get('/api/users', controllerAccueil );
 
--- routing/app-routes.js
+++ routing/app-routes.js
@@ -3,19 +3,19 @@ const { resolve } = require('path');
 
 const router = require('express').Router();
 
-const controllerApp = require('../controllers/controller.js');
+const {controllerContact, controllerServices,controllerAccueil, controllerDefault} = require('../controllers/controller.js');
  
  /*/ Créer les routes en GET permettant de charger les fichier */
-router.get('/contact', controllerApp.controllerContact );
+router.get('/contact', controllerContact );
 
-router.get('/services', controllerApp.controllerServices );
+router.get('/services', controllerServices );
 
-router.get('/index', controllerApp.controllerAccueil )
+router.get('/index',  controllerAccueil )
  
-router.get('*', controllerApp.controllerDefault );
+router.get('*',  controllerDefault );
 
 //add the router
- 
+  
 
 
 module.exports = router;
