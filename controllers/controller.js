7eb301834a02a010445f9c4b3aae9f511df314a1
Fekhreddine23 <fekhredine1990@gmail.com>
2022-12-23 15:36:28 +0100

    creation des view , changement des controlleurs pour acces aux views en js but alleger les requetes avec le serveur

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,21 +2,26 @@ const { resolve } = require('path');
 
 
 const controllerContact = function (req, res) {
-    res.sendFile(resolve('public', 'contact.html'));
+    exports.usersPage = (req, res) => {
+        res.send(require('../views/contact'));
+      }
 };
 
 const controllerServices = function (req, res) {
-    res.sendFile(resolve('public', 'services.html'));
+    //res.sendFile(resolve('public', 'services.html')); //chemin pour dossier public 
+    res.send(require('../views/services')); //chemin pour generer les vues dans le dossier view
 };
 
 const controllerAccueil = function (req, res) {
-    res.sendFile(resolve('public', 'index.html'));
+   // res.sendFile(resolve('public', 'index.html'));
+   res.send(require('../views/index'));
 };
 
  
 
 const controllerDefault = function (req, res) {
-    res.sendFile(resolve('public', 'page404.html'));
+    //res.sendFile(resolve('public', 'page404.html'));
+    res.send(require('../views/page404'));
 };
 
 module.exports = {
---
+++ views/index.js
@@ -0,0 +1,24 @@
+module.exports.html = `<body>
+
+    <h1> Accueil </h1>
+
+    <nav>
+
+        <a href="/home"> Accueil</a>
+        <a href="/contact">Contact</a>
+        <a href="/services">Service</a>
+
+    </nav>
+
+    <table>
+
+        <thead>
+            <tr>
+                <th>utilisateurs</th>
+
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>`
